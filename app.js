70c6ee2bad93e8ba2c8db4a0aef9fa9358d5bd1e
vickybird <victoria.er.bird@gmail.com>
2014-03-22 17:39:51 +0000

    Registration checks for pre-existing username.

--- app.js
+++ app.js
@@ -30,17 +30,29 @@ app.get('/register', function(req, res) {
 });
 
 app.post('/register', function(req, res) {
-  var newAccount = new Account({
-    username: req.body.username,
-    password: req.body.password,
-    firstName: '',
-    surname: ''
-  });
-  newAccount.save(
-    function(err, account) {
+  Account.find({ name: /req.body.username/ },
+    function(err, accounts) {
       if (err) { throw err; }
-      console.log('New account added: ' + newAccount);
-      res.redirect('/account');
+      if (accounts) {
+        console.log('User already exists');
+        res.render('register.jade', {
+          userExists: true,
+          username: req.body.username
+        });
+      } else {
+        var newAccount = new Account({
+          username: req.body.username,
+          password: req.body.password,
+          firstName: '',
+          surname: ''
+        });
+        newAccount.save(
+          function(err, account) {
+            if (err) { throw err; }
+            console.log('New account added: ' + newAccount);
+            res.redirect('/account');
+          });
+      }
     });
 });
 

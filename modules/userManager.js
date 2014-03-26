71ca09e47a43601233b741279c9a0ef4d5b33ac7
vickybird <victoria.er.bird@gmail.com>
2014-03-26 11:26:10 +0000

    Corrected usage of signed cookie for authentication to allow persistence of login.

--- modules/userManager.js
+++ modules/userManager.js
@@ -1,3 +1,5 @@
+var Accounts = require('models/account.js');
+
 exports.serializeUser = function(user, done) {
   done(null, user);
 };
@@ -7,8 +9,7 @@ exports.deserializeUser = function(obj, done) {
 };
 
 exports.login = function(req, res, user, next) {
-  req.login(user, {}, function(err) {
-    if (err) { throw err; }
+  login(req, user, function() {
     res.cookie('user', user.username, { signed: true });
     next();
   });
@@ -21,17 +22,38 @@ exports.logout = function(req, res, next) {
 
 exports.ensureAuthenticated = function(req, res, next) {
   res.cookie('redirectTo', req.path);
-  if (req.isAuthenticated() && verifyCookie(req, req.user.username)) {
-    return next();
+  console.log(verifyCookie(req));
+  if (verifyCookie(req)) {
+    if (!req.isAuthenticated()) {
+      Accounts.find(
+        { username: req.signedCookies.user },
+        function(err, accounts) {
+          if (err) { throw err; }
+          if (accounts === 0) {
+            throw 'No account found.';
+          }
+          login(req, accounts[0], next);
+        });
+    } else {
+      next();
+    }
+  } else {
+    res.redirect('/login');
   }
-  res.redirect('/login');
 };
 
 // 
 // Private Functions
 //
 
+function login(req, user, callback) {
+  req.login(user, {}, function(err) {
+    if (err) { throw err; }
+    callback();
+  });
+}
+
 function verifyCookie(req, username) {
-  if (req.signedCookies.user === username) { return true; }
+  if (req.signedCookies.user) { return true; }
   return false;
 }

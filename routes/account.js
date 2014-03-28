434a68822e866894a4aa20f0a09404ddadc726ae
vickybird <victoria.er.bird@gmail.com>
2014-03-28 11:07:02 +0000

    Added password changing.

--- routes/account.js
+++ routes/account.js
@@ -5,4 +5,30 @@ module.exports = function(app) {
     res.render('account.jade',
       { user: req.user.firstName });
   });
+
+  app.get('/changepassword', userManager.ensureAuthenticated, function(req, res) {
+    res.render('changePassword.jade');
+  });
+
+  app.post('/changepassword', userManager.ensureAuthenticated, function(req, res) {
+    req.user.authenticate(req.body.oldPassword, function(err, user, info) {
+      if (err) { throw err; }
+      if (!user) {
+        res.render('changePassword.jade', { message: info.message });
+      } else {
+        if (req.body.newPassword != req.body.newPassword2) {
+          res.render('changePassword.jade', { message: 'New passwords don\'t match.' });
+        } else {
+          req.user.setPassword(req.body.newPassword, function(err, account) {
+            if (err) {
+              res.render('changePassword.jade', { message: err.message });
+            } else {
+              account.save();
+              res.render('changePassword.jade', { message: 'Password successfully changed!' });
+            }
+          });
+        }
+      }
+    });
+  });
 };
